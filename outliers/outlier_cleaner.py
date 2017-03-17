0d5101aa9d46cce211d6204f68745e8d75db31a6
Alireza Omidi <alireza530@gmail.com>
2017-03-17 15:09:40 +0330

    Add K-Means

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,11 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
 
     ### your code goes here
-
+    errors = [abs(p-n) for p,n in zip(predictions, net_worths)]
+    cleaned_data = sorted([(a,n,e) for a,n,e in zip(ages, net_worths, errors)], key=lambda i: i[2])
+    cleaned_data = cleaned_data[:len(cleaned_data)/10*9]
     
     return cleaned_data
 
